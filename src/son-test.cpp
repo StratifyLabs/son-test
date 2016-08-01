dce42ef3d504453d39506a64ea97a2f0547a0223
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2016-07-31 22:22:57 -0600

    son-test first commit

---
+++ src/son-test.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : son-test.cpp
+// Author      : Tyler G
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#include <stfy/son.h>
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
